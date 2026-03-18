f7fdf82dbc049694654031ad193b59bce13f61d3
SXsid <sidshekhar2024@gmail.com>
2026-03-19 01:15:53 +0530

    json formatter

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,7 +1,40 @@
+//go:build js && wasm
+
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
 	fmt.Println("heree i came wasm i am the osm")
+	js.Global().Set("FormatJson", formatJsonWrapper())
+	select {}
+}
+
+func formatJsonWrapper() js.Func {
+	return js.FuncOf(func(_ js.Value, args []js.Value) any {
+		if len(args) != 1 {
+			return "Invalid number of argumnets"
+		}
+		prettryJson, err := preetify(args[0].String())
+		if err != nil {
+			return err.Error()
+		}
+		return prettryJson
+	})
+}
+
+func preetify(input string) (string, error) {
+	var data any
+	if err := json.Unmarshal([]byte(input), &data); err != nil {
+		return "", err
+	}
+	pretty_json, err := json.MarshalIndent(data, "", "	")
+	if err != nil {
+		return "", err
+	}
+	return string(pretty_json), nil
 }
